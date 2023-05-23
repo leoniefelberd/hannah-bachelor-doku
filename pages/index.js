9fcb40684bf9ad839fda3a4eaa6b5454b5334e1f
leoniefelberd <75806511+leoniefelberd@users.noreply.github.com>
2023-05-23 14:45:20 +0200

    backend changes

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,65 @@
 import Head from "next/head"
 import { Component } from 'react'
 import styles from "../styles/Home.module.scss";
+import home from "../content/home.json"
+
+import ReactPlayer from 'react-player';
 
 // import { attributes, react as HomeContent } from '../content/home.md'
 
-export default class Home extends Component {
-  render() {
-    // let { title, cats } = attributes
-    return (
-      <>
-        <Head>
-          <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
-        </Head>
+export default function Home(props) {
 
-        hello hannah doku
-        {/* <article>
+    console.log("home", home);
+
+
+        // let { title, cats } = attributes
+        return (
+            <>
+                <Head>
+                    <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+                </Head>
+
+                hello hannah doku
+
+                <div>
+                    {home.galery.map((slideImage, k) => (
+                        <div key={k}>
+                            {
+                                slideImage.type === 'video' &&
+                                <div className="each-slidevideo" key={k}>
+                                    video
+                                    <ReactPlayer 
+                                    url={slideImage.videolink}
+                                    controls={true}
+                                    />
+                                    {/* <ReactPlayer
+                                        className={styles.slidevideo}
+                                        url={slideImage.videolink}
+                                        controls={false}
+                                        width='100%'
+                                        height='100%'
+                                        playing={props.playonclick}
+                                        pip={true}
+                                        autoPlay={false}
+                                    // onClick={props.setPlay}
+                                    // onClick={() => props.setPlay(true)}
+                                    // onPlay={() => props.setPlay(true)}
+                                    /> */}
+                                </div>
+                            }
+                            {
+                                slideImage.type === 'image' &&
+                                <div className="each-slide" key={k}>
+                                    <img
+                                        className={styles.slide}
+                                        src={slideImage.image}
+                                    />
+                                </div>
+                            }
+                        </div>
+                    ))}
+                </div>
+                {/* <article>
           <h1>{title}</h1>
           <HomeContent />
           <ul>
@@ -26,7 +71,6 @@ export default class Home extends Component {
             ))}
           </ul>
         </article> */}
-      </>
-    )
-  }
-}
+            </>
+        )
+    }
