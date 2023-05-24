2940b0f582c2bd77b0f98d27b61c6cf9b27a7481
leoniefelberd <75806511+leoniefelberd@users.noreply.github.com>
2023-05-24 22:22:37 +0200

    ohne video

--- pages/index.js
+++ pages/index.js
@@ -190,7 +190,7 @@ export default function Home(props) {
                                     <div className={styles.eachslidevideo}
                                         key={index}
                                     >
-                                        
+
                                         {/* <iframe src="https://player.vimeo.com/video/81007030?h=62949ac305&color=ffffff&title=0&byline=0&portrait=0" width="640" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
                                         <p><a href="https://vimeo.com/81007030">ECAL/Photography - Class of Sannwald</a> from <a href="https://vimeo.com/ecal">ECAL</a> on <a href="https://vimeo.com">Vimeo</a>.</p> */}
                                         {/* <ReactPlayer
@@ -199,7 +199,7 @@ export default function Home(props) {
                                             onMouseOver={event => event.target.play()}
                                             onMouseOut={event => event.target.pause()}
                                         /> */}
-                                        <video
+                                        {/* <video
                                             // poster="https://i.imgur.com/Us5ckqm.jpg"
                                             // onMouseOver={e => e.target.play()}
                                             onMouseOut={e => e.target.pause()}
@@ -208,7 +208,11 @@ export default function Home(props) {
                                             width="600px"
                                             // height="800px"
                                         >
-                                        </video>
+                                        </video> */}
+                                        {/* <video controls width="100%">
+                                            <source src={slideImage.videolink} type="video/mp4" />
+                                            Sorry, your browser doesn't support embedded videos.
+                                        </video> */}
                                         {/* <ReactPlayer
                                             url="https://vimeo.com/3155182"
                                         /> */}
