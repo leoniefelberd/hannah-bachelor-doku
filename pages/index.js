f03d397fc2690b0c5be7b11663f919377a2d97ac
HannahBoldt <75832890+HannahBoldt@users.noreply.github.com>
2023-06-10 20:43:07 +0200

    Update index.js
    Update Filter

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,8 @@ export default function Home(props) {
 
 
     //FILTER
-    const allKategories = ["Zeit", "Zeitung", "Zeiger", "Zeneggen", "Uhr", "Testen", "Sammeln", "Schatten", "Schreiben", "Sonne", "Struggle", "Recherchieren", "Projizieren", "Natur", "Notizen", "Miro", "Lesen", "Loading bar", "Kalender", "Gefundenes", "Experimentieren", "Beobachten", "Ausstellen"];
+    const allKategories = ["Zeit", "Zeitung", "Zeiger", "Zeneggen", "Uhr", "Testen", "Sammeln", "Schatten", "Schreiben", "Sonne", "Recherchieren", "Projizieren", "Natur", "Notizen", "Miro", "Lesen", "Loading bar", "Kalender", "Gefundenes", "Fail", "Endspurt
+                         ", "Experimentieren", "Beobachten", "Ausstellen"];
     const [filter, setFilter] = useState(["Zeit"])
 
 
